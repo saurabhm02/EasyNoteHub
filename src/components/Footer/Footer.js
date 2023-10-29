00055740b0cfba04767bd3545a5d9ac90475d603
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-10-29 13:24:25 +0530

    Footer file added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        &copy; 2023  <a href="https://github.com/saurabhm02" target="_blank">Saurabh Mahapatra👨‍💻</a>
+    </div>
+  )
+}
+
+export default Footer;
