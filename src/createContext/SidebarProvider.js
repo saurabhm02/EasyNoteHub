fc2a29cf93ae758ad829a29cbc453f5b2c529652
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-10-29 13:22:50 +0530

    Sidebar context file added

---
+++ src/createContext/SidebarProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext } from "react";
+import { useContext, useState } from "react";
+
+const SidebarContext = createContext();
+export const SidebarProvider=(props)=>{
+    const [sidebar, setSidebar] = useState(true);
+    
+    return(
+        <SidebarContext.Provider value={{sidebar, setSidebar}}>
+            {props.children}
+        </SidebarContext.Provider>
+    )
+};
+
+export const useSidebar =() => useContext(SidebarContext);
