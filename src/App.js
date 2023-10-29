e9c759a2a9df2d3b4f23d8f05f435d3a3bc5e114
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2023-10-29 13:17:50 +0530

    Some modifications

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CreateNote from './components/CreateNote/CreateNote';
+import React from 'react';
+import { PageProvider } from './createContext/PageProvider';
+import { SidebarProvider } from './createContext/SidebarProvider';
+import NavBar from './components/NavBar/NavBar';
+import SideBar from './components/SideBar/SideBar';
+import { NoteProvider } from './createContext/NoteProvider';
+import RenderNotes from './page/RenderNotesPage/RenderNotes';
+import Archived from './page/ArchivedPage/Archived';
+import Trash from './page/TrashPage/Trash';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageProvider>
+        <SidebarProvider>
+          <NavBar />
+          <SideBar />
+          <div className="content">
+            <NoteProvider>
+              <CreateNote />
+              <Routes>
+                <Route path="/" element={<RenderNotes />} />
+                <Route path="/archived" element={<Archived />} />
+                <Route path="/trash" element={<Trash />} />
+              </Routes>
+            </NoteProvider>
+          </div>
+          <Footer />
+        </SidebarProvider>
+      </PageProvider>
     </div>
   );
 }
